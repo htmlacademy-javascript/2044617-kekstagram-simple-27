29a84e9edeb56246bfcfed3f46a61aac4772b5e0
GreenFest-Horus <103495357+GreenFest-Horus@users.noreply.github.com>
2022-10-04 23:29:11 +0300

    Добавлят 2 функции по второму заданию

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно. Пример функции:
+function getRandomIntInclusive(min, max) {
+  if (min >= 0 && max >= 0 && min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  else{
+    return NaN;
+  }
+}
+
+//взято c https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+//Функция для проверки максимальной длины строки:
+function checkMaxStringLength (string, maxLength) {
+  return string.length <= maxLength;
+}
