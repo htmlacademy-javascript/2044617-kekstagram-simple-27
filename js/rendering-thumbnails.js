e28e60e5cae8b2104a527732471bb2903a97c169
GreenFest-Horus <103495357+GreenFest-Horus@users.noreply.github.com>
2022-11-05 01:35:23 +0300

    Добавляет отображение фотографий других пользователей

---
+++ js/rendering-thumbnails.js
@@ -0,0 +1,33 @@
+import {getRandomPhotoDescription} from './create-random-array.js';
+
+//Находим контайнер для изображения других пользователей
+const picturesContainer = document.querySelector('.pictures');
+
+// шаблон изображения случайного пользователя
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
+
+//создание массива с заполенными в них данными
+const arrayData = getRandomPhotoDescription();
+
+const getThumbnails = function() {
+
+  //создание фрагмента для последующей отрисовки миниатюр
+
+  const renderingListFragment = document.createDocumentFragment();
+
+  //Выбираем из массива нужные нам данные и записываем в фрагмент
+  arrayData.forEach(({url, description, likes, comments}) => {
+    const photography = templatePicture.cloneNode(true);
+    photography.querySelector('.picture__img').src = url;
+    photography.querySelector('.picture__img').alt = description;
+    photography.querySelector('.picture__comments').textContent = comments;
+    photography.querySelector('.picture__likes').textContent = likes;
+
+    renderingListFragment.append(photography);
+  });
+
+  picturesContainer.append(renderingListFragment);
+
+};
+
+export {getThumbnails};
