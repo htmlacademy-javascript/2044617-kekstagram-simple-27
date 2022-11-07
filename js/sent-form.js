1b0d28f16d8dc6e810cd229f41de50920774a5a0
GreenFest-Horus <103495357+GreenFest-Horus@users.noreply.github.com>
2022-11-07 22:37:27 +0300

    Добавляет обработчики в соответствии с тех.заданием

---
+++ js/sent-form.js
@@ -0,0 +1,45 @@
+import {isEscapeKey} from './util.js';
+
+//const MIN_COMMENTH_LENGTH = 20;
+//const MAX_COMMENTH_LENGTH = 140;
+//const commentField = document.querySelector('.text__description');
+//const form = document.querySelector('.img-upload__form');
+const uploadFileInput = document.querySelector('#upload-file');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const closeUploadOverlayElement = document.querySelector('#upload-cancel');
+
+
+//Функция проверяет нажатия Esc и закрывает окно загрузки фото
+const onUploadOverlayEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUploadOverlay();
+  }
+};
+//Функция открывает окно загрузки фото
+const openUploadOverlay = function () {
+  uploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onUploadOverlayEscKeydown);
+};
+
+//Функция закрывает окно загрузки фото
+
+function closeUploadOverlay () {
+  uploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onUploadOverlayEscKeydown);
+  uploadFileInput.value = '';
+}
+
+//Добавляет обработчик событий - при загрузке фото, открывается окно
+uploadFileInput.addEventListener('change', () => {
+  openUploadOverlay();
+});
+
+//Добавляет обработчик событий  - при нажатии на кнопку закрытия окна, оно закрывается
+closeUploadOverlayElement.addEventListener('click', () => {
+  closeUploadOverlay();
+});
