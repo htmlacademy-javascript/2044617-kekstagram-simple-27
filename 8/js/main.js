1704d21623f832891cb2e8360fabdbce26923e66
Keks <keks@htmlacademy.ru>
2022-11-21 18:19:54 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/main.js
+++ 8/js/main.js
@@ -1,6 +1,6 @@
-import {getThumbnails} from './rendering-thumbnails.js';
-import {formSubmit, onSuccessfulSending, onFailSending} from './sent-form.js';
-import {getData} from './fetch.js';
+import { getThumbnails } from './rendering-thumbnails.js';
+import { formSubmit, onSuccessfulSending, onFailSending } from './sent-form.js';
+import { getData } from './fetch.js';
 import './sent-form.js';
 import './scaling.js';
 import './effects.js';
@@ -9,4 +9,6 @@ import './effects.js';
 formSubmit(onSuccessfulSending, onFailSending);
 
 //Вызов функции для получения фото других пользователей
-getData(getThumbnails);
+getData((photos) => {
+  getThumbnails(photos);
+});
